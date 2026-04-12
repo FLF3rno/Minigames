30df39b1a72f015b43c028c1d2ea50ad731e2da5
FLF3rno <gelatovariegatonutella@gmail.com>
2026-04-12 15:27:57 +0200

    changed text

--- src/main/java/net/mcreator/minigames/procedures/NextLayerDestroyedInDisplayProcedure.java
+++ src/main/java/net/mcreator/minigames/procedures/NextLayerDestroyedInDisplayProcedure.java
@@ -6,6 +6,6 @@ import net.mcreator.minigames.network.MinigamesModVariables;
 
 public class NextLayerDestroyedInDisplayProcedure {
 	public static String execute(LevelAccessor world) {
-		return "The top layer is conquered in " + new java.text.DecimalFormat("##").format((400 - MinigamesModVariables.MapVariables.get(world).layerCountdownSpleef) / 20) + "s";
+		return "The top layer is destroyed in " + new java.text.DecimalFormat("##").format((400 - MinigamesModVariables.MapVariables.get(world).layerCountdownSpleef) / 20) + "s";
 	}
 }
